9df84808fe45504c8bdf5d0ffddc57886d987749
Geovanni Venerando <geovanni_venerando@live.com>
2017-06-12 04:52:14 -0300

    Primeira Tela

--- app/src/main/java/cielo/projeto/pravoce/MainActivity.java
+++ app/src/main/java/cielo/projeto/pravoce/MainActivity.java
@@ -1,13 +1,44 @@
 package cielo.projeto.pravoce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botaoum;
+    private Button botaodois;
+    private Button botaotres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoum = (Button) findViewById(R.id.idBotaoUm);
+        botaodois = (Button) findViewById(R.id.idBotaoDois);
+        botaotres = (Button) findViewById(R.id.idBotaoTres);
+
+        botaoum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Aluno.class));
+            }
+        });
+
+        botaodois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Aluno.class));
+            }
+        });
+
+        botaotres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Aluno.class));
+            }
+        });
     }
 }
