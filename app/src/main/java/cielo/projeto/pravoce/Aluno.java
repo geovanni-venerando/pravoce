f4f61b5c6b4f3d25a232c25bce9c5a0d8e54eba8
Geovanni Venerando <geovanni_venerando@live.com>
2017-06-13 01:53:51 -0300

    Terceira Tela

--- app/src/main/java/cielo/projeto/pravoce/Aluno.java
+++ app/src/main/java/cielo/projeto/pravoce/Aluno.java
@@ -1,13 +1,27 @@
 package cielo.projeto.pravoce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Aluno extends AppCompatActivity {
+
+    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aluno);
+
+        botao = (Button) findViewById(R.id.bntEntrar);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Aluno.this, MainActivity.class));
+            }
+        });
     }
 }
